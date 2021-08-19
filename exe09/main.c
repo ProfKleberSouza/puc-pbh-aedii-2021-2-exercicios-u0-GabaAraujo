48786a195ba2d8201d391e134acd5c3486c8c468
Gabriel Morais Batista Assis Araujo <gabamorais@outlook.com>
2021-08-19 18:24:07 -0300

    at7
    at7

--- exe09/main.c
+++ exe09/main.c
@@ -9,7 +9,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
-#define MAX 5
+#define MAX 100
 
 typedef struct Aluno
 {
@@ -46,16 +46,30 @@ int main() {
         
     }
 
-    float aux = 0;
-    char aux_string[100] ;
+
+
+    Aluno auxiliar;
 
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
 
-            if(aluno[i].nota < aluno[j].nota)
+            if(aluno[i].nota > aluno[j].nota)  //decrescente 
             {
+
+
+              //if(strcmp(aluno[i].nome,aluno[j].nome) < 0) // 0 = nomes iguais //  < 0 = aluno[i].nome vem antes que aluno[j] // >0 segundo menor que o primeiro
+              // {
+                auxiliar = aluno[i];
+                aluno[i] = aluno[j];
+                aluno[j] = auxiliar;
+            //   }
+
+
+
+
+                /*
                  aux = aluno[j].nota;
                  strcpy(aux_string,aluno[j].nome);
 
@@ -65,6 +79,7 @@ int main() {
                  aluno[i].nota = aux;
                  strcpy(aluno[j].nome,aux_string);
 
+                */
 
             }
 
@@ -72,19 +87,48 @@ int main() {
 
 
     }
+        
+      //  for(int i=0;i<n;i++)
+     //   {
+     //        printf("%s %.1f", aluno[i].nome,aluno[i].nota); //1maior
+     //   }
 
-    
-    
+      
+ 
+
+    for(int i=0;i<3;i++) //compara os 3 maiores em ordem alfabetica
+    {
+        for(int j=0;j<3;j++)
+        {
+
+          
+              if(strcmp(aluno[i].nome,aluno[j].nome) < 0) // 0 = nomes iguais //  < 0 = aluno[i].nome vem antes que aluno[j] // >0 segundo menor que o primeiro
+               {
+                auxiliar = aluno[j];
+                aluno[j] = aluno[i];
+                aluno[i] = auxiliar;
+               }
+
+                    //1 em ordem alfabetica das 3 maiores notas 
+
+        }
+
+
+    }
+
+
+
     printf("NOTA MEDIA = %.1f", media/n);
     printf("\n");
-    printf("%s %.1f", aluno[n-3].nome,aluno[n-3].nota); //3maior
+    printf("%s %.1f", aluno[0].nome,aluno[0].nota); //1maior
      printf("\n");
-    printf("%s %.1f", aluno[n-2].nome,aluno[n-2].nota); //2maior
+    printf("%s %.1f", aluno[1].nome,aluno[1].nota); //2maior
      printf("\n");
-    printf("%s %.1f", aluno[n-1].nome,aluno[n-1].nota); //1maior
+    printf("%s %.1f", aluno[2].nome,aluno[2].nota); //3maior
      printf("\n");
   
-
+    
 
 
 }
+
